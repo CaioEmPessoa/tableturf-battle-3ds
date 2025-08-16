5e6e9745974a2071af33e4477f5a657979781cac
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-08-16 19:10:29 -0300

    Removed unused varkables and bring back global touchPosition

--- src/global.h
+++ src/global.h
@@ -10,7 +10,6 @@
 // macros
 #define ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
 #define UNUSED(x) (void)(x)
-
 // board elements
 static int maxTileX = 10; // set tile boundaries
 static int maxTileY = 10;
@@ -18,9 +17,8 @@ static int maxTileY = 10;
 static int topSqrSize = 10; // top screen sqr size
 static int btnSqrSize = 10; // bottom screen sqr size
 
-static int playerTileX = 1; // player positioning
-static int playerTileY = 1;
-
 static bool cursorBot = false;
 
 static char slctScreen; // screen that is being drawn on
+
+static touchPosition touch; // touch coords, touch.px & touch.py
