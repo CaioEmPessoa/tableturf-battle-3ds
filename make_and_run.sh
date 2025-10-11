d63411e5cfcbd6767bae976ba3de79831ccc95d8
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-10-10 23:39:00 -0300

    Updated make_and_run script to add citra test option

--- make_and_run.sh
+++ make_and_run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-DSIP='YUORIP'
 FILENAME='tableturf-battle'
+FILETYPE='3dsx'
+RUN_ON_CITRA=false # run on citra or original hardware
+DSIP='YUORIP' # 192.168.XXX.XXX (without port) (needed if on original hardware)
+CITRA_PATH='path/to/citra.app' # (needed if on citra)
 
 make
 
-3dslink $FILENAME-3ds.3dsx -a $DSIP
+if [ $RUN_ON_CITRA == false ]; then
+    3dslink $FILENAME-3ds.$FILETYPE -a $DSIP
+else
+    "$CITRA_PATH" "./$FILENAME-3ds.$FILETYPE"
+fi
