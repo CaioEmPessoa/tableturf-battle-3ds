095319004c5e3be1c35685a503a5f470285f6167
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-07-16 16:05:11 -0300

    Added new function to drawText

--- src/3ds-easy-lib/drawing.h
+++ src/3ds-easy-lib/drawing.h
@@ -5,8 +5,8 @@
 int* drawSquare(int x, int y, int w, int h, u32 c)
 {
 	C2D_DrawRectangle(
-		convertPos('w', x, 0),
-		convertPos('h', y, 0)-h,
+		convertPos('w', x),
+		convertPos('h', y)-h,
 		0, w, h,
 		c, c, c, c
 	);
@@ -15,4 +15,21 @@ int* drawSquare(int x, int y, int w, int h, u32 c)
     int *pResult = malloc(sizeof(int) * 4);
     memcpy(pResult, result, sizeof(int) * 4);
     return pResult;
+}
+
+int* drawText(float x, float y, char* text, float fontSize) {
+	C2D_Text txt;
+	C2D_TextBuf textBuffer = C2D_TextBufNew(128); // TODO: Change to dynamic value
+
+	C2D_TextParse(&txt, textBuffer, text);
+	C2D_TextOptimize(&txt);
+
+	C2D_DrawText(
+		&txt, 0,
+		convertPos('w', x), convertPos('y', y),
+		0.0f,
+		fontSize, fontSize
+	);
+
+	return 0;
 }
